be41ac21e301964fdadba44e1c451ab48df697c0
Martial Escudero <martial.escudero@gmail.com>
2022-03-09 16:15:23 +0100

    sentiers request

---
+++ pages/map.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="sentier in sentiers" :key="sentier.item">
+      <p>{{ sentier.attributes.Nom }}</p>
+      <p>{{ sentier.attributes.Description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  head: {
+    title: 'Carte',
+  },
+  methods:{
+    ...mapActions('store',['getSentiers']),
+  },
+  computed: {
+    ...mapGetters('store',['sentiers'])
+  },
+  mounted() {
+    this.getSentiers()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- store/store.js
+++ store/store.js
@@ -1,15 +1,30 @@
 import axios from 'axios'
 
-const strapi = ""
+const strapi = "https://admingreencampus.herokuapp.com/api/"
 
-export const state = () => ({  
+export const state = () => ({
+  sentiers: []
 })
 
 export const mutations = {
+  setSentiers(state, args) {
+    state.sentiers = args.data
+  }
 }
 
 export const actions = {
+  getSentiers({commit}) {
+    axios.get(strapi + 'sentiers?populate=*')
+    .then((res) => {
+      commit('setSentiers', res.data)
+      console.log(res)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+  }
 }
 
 export const getters = {
+  sentiers : state => state.sentiers
 }
